b5b359b0daf7acabfab39a66acb0277ae0fb6797
nico pleyer <npleyer.np@gmail.com>
2021-11-15 21:56:13 +0100

    generate category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_204317) do
+ActiveRecord::Schema.define(version: 2021_11_15_205449) do
 
   create_table "bookings", force: :cascade do |t|
     t.string "category"
     t.decimal "amount"
     t.date "booked_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
