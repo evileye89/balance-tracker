34b3a093940a8eabf602fea1b15da95e3c9ce74e
nico pleyer <npleyer.np@gmail.com>
2021-12-14 23:09:09 +0100

    add new booking form view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,4 +6,24 @@ class BookingsController < ApplicationController
   def show
     @booking = Booking.find(params[:id])
   end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:amount, :category_id)
+  end
 end
