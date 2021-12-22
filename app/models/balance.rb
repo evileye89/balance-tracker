ff16936890023c8d65071dbf7db2baccf98819e0
nico pleyer <npleyer.np@gmail.com>
2021-12-22 17:50:12 +0100

    add account name to balance view

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,4 +1,8 @@
 class Balance < ApplicationRecord
+    belongs_to :account, foreign_key: :account_id
+    
+    includes(:account)
+    
     validates :balance, presence: true
     validates :year, presence: true
     validates :month, presence: true
