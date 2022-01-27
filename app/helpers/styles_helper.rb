6c89321a023cdfc885fe0790ab58ba7bbe6939d2
nico pleyer <npleyer.np@gmail.com>
2022-01-27 18:28:29 +0100

    change table hover styles

--- app/helpers/styles_helper.rb
+++ app/helpers/styles_helper.rb
@@ -1,6 +1,6 @@
 module StylesHelper
     def css_currency_bg(number)
         return if number == 0
-        number.positive? ? 'bg-success' : 'bg-danger'
+        number.positive? ? 'bg-positive' : 'bg-negative'
     end
 end
