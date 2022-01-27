91b8ddff9b0adee6860617c7e1eaf76e52d702d4
nico pleyer <npleyer.np@gmail.com>
2022-01-27 17:44:48 +0100

    refactor calculations on account view

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,19 @@
 module AccountsHelper
+    def calc_balances_per_month(month, balances_per_year)
+        value = balances_per_year.find_by(:month => month)
+        balance = value.present? ? value.balance : 0
+    end
+
+    def calc_balances_per_year(balances_per_year)
+        balances_per_year.inject(0) { |sum, item| sum + item.balance }
+    end
+
+    def calc_category_per_month(month, bookings)
+        bookings_per_month = bookings.select { |booking| booking.booked_at.mon == month}
+        bookings_per_month.inject(0) { |sum, item| sum + item.amount }
+    end
+
+    def calc_category_per_year(bookings)
+        bookings.inject(0) { |sum, item| sum + item.amount } 
+    end
 end
